411ae3997b1bd13a4ca9de31aca8db9e712d9ae8
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-06-10 19:28:03 -0400

    Correct mistakes in mermaid class

--- lib/mermaid.rb
+++ lib/mermaid.rb
@@ -1,15 +1,17 @@
 class Mermaid
   attr_reader :name, :age
 
-  def initialize(name, age)
+  def initialize(name)
     @name = name
     @age = 22
   end
 
   def get_older
-    @age +1
+    @age = @age +1
   end
 end
 
-mermaid1 = Mermaid.new("Sereia, 22")
+mermaid1 = Mermaid.new("Sereia")
 mermaid1.get_older
+
+p mermaid1
